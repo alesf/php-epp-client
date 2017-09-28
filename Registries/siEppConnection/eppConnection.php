6dfd3663019745d6092e5de297d002cf22f38335
Aleš Farčnik <far.c.nik+git@gmail.com>
2017-09-28 09:32:01 +0200

    Slovenian Arnes registy
    added .si registry

---
+++ Registries/siEppConnection/eppConnection.php
@@ -0,0 +1,17 @@
+<?php
+namespace Metaregistrar\EPP;
+
+class siEppConnection extends eppConnection
+{
+    public function __construct($logging = false, $settingsfile = null)
+    {
+        parent::__construct($logging, $settingsfile);
+        parent::setServices(array('urn:ietf:params:xml:ns:domain-1.0' => 'domain', 'urn:ietf:params:xml:ns:host-1.0' => 'host', 'urn:ietf:params:xml:ns:contact-1.0' => 'contact'));
+        parent::addExtension("dnssi", "http://www.arnes.si/xml/epp/dnssi-1.2");
+        parent::addExtension("registrar", "http://www.arnes.si/xml/epp/registrar-1.0");
+        parent::addExtension("DNScheck", "http://www.arnes.si/xml/epp/DNScheck-1.0");
+        parent::setLanguage('en-US');
+        parent::setVersion('1.0');
+        parent::enableDnssec();
+    }
+}
