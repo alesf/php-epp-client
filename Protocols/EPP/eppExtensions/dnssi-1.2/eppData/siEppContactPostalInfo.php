75454fa0b63dbe94810f4026a527d83a367c41ed
Aleš Farčnik <far.c.nik+git@gmail.com>
2017-10-02 15:04:05 +0200

    Update Slovenian EPP commands
    ContactPostalInfo
    CreateContactRequest
    CreateDomainRequest

---
+++ Protocols/EPP/eppExtensions/dnssi-1.2/eppData/siEppContactPostalInfo.php
@@ -0,0 +1,76 @@
+<?php
+namespace Metaregistrar\EPP;
+
+class siEppContactPostalInfo extends eppContactPostalInfo
+{
+    const ARNES_CONTACT_TYPE_PERSON = 'person';
+    const ARNES_CONTACT_TYPE_ORG = 'org';
+
+    private $contactType;
+    private $contactID;
+
+    /**
+     *
+     * @param string $name
+     * @param string $city
+     * @param string $countrycode
+     * @param string $organisationName
+     * @param string $street
+     * @param string $province
+     * @param string $zipcode
+     * @param string $type POSTAL_TYPE_LOCAL or POSTAL_TYPE_INTERNATIONAL
+     */
+    public function __construct(
+        $name = null,
+        $city = null,
+        $countrycode = null,
+        $organisationName = null,
+        $street = null,
+        $province = null,
+        $zipcode = null,
+        $type = self::POSTAL_TYPE_INTERNATIONAL
+    ) {
+        parent::__construct($name, $city, $countrycode, $organisationName, $street, $province, $zipcode, $type);
+
+        $this->contactType = self::ARNES_CONTACT_TYPE_PERSON;
+    }
+
+    /**
+     *
+     * @param type $type person or org
+     */
+    public function setContactType($type)
+    {
+        $type = strtolower($type);
+        if (($type != self::ARNES_CONTACT_TYPE_PERSON) && ($type!=self::ARNES_CONTACT_TYPE_ORG)) {
+            throw new eppException('Person type can only be '.self::ARNES_CONTACT_TYPE_PERSON.' or '.self::ARNES_CONTACT_TYPE_ORG);
+        }
+        $this->contactType = $type;
+    }
+
+    public function getContactType()
+    {
+        return $this->contactType;
+    }
+
+    public function setContactID($id)
+    {
+        $this->contactID = $id;
+    }
+
+    public function getContactID()
+    {
+        return $this->contactID;
+    }
+
+    public function getIDType()
+    {
+        switch ($this->getContactType()) {
+            case self::ARNES_CONTACT_TYPE_ORG:
+                return "maticna";
+            case self::ARNES_CONTACT_TYPE_PERSON:
+            default:
+                return "EMSO";
+        }
+    }
+}
--- Registries/siEppConnection/eppConnection.php
+++ Registries/siEppConnection/eppConnection.php
@@ -6,12 +6,21 @@ class siEppConnection extends eppConnection
     public function __construct($logging = false, $settingsfile = null)
     {
         parent::__construct($logging, $settingsfile);
-        parent::setServices(array('urn:ietf:params:xml:ns:domain-1.0' => 'domain', 'urn:ietf:params:xml:ns:host-1.0' => 'host', 'urn:ietf:params:xml:ns:contact-1.0' => 'contact'));
+        parent::setServices(array(
+            'urn:ietf:params:xml:ns:domain-1.0' => 'domain',
+            'urn:ietf:params:xml:ns:host-1.0' => 'host',
+            'urn:ietf:params:xml:ns:contact-1.0' => 'contact'
+        ));
         parent::addExtension("dnssi", "http://www.arnes.si/xml/epp/dnssi-1.2");
         parent::addExtension("registrar", "http://www.arnes.si/xml/epp/registrar-1.0");
         parent::addExtension("DNScheck", "http://www.arnes.si/xml/epp/DNScheck-1.0");
         parent::setLanguage('en-US');
         parent::setVersion('1.0');
+
         parent::enableDnssec();
+
+        parent::addCommandResponse('Metaregistrar\EPP\siEppCreateDomainRequest', 'Metaregistrar\EPP\eppCreateDomainRequest');
+        parent::addCommandResponse('Metaregistrar\EPP\siEppCreateContactRequest', 'Metaregistrar\EPP\eppCreateContactRequest');
+        parent::addCommandResponse('Metaregistrar\EPP\siEppContactPostalInfo', 'Metaregistrar\EPP\eppContactPostalInfo');
     }
 }
