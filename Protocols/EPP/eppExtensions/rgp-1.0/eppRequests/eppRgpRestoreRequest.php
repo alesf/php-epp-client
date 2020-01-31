afb68aeb2627ca0d83f6f503c253febd81150782
alesf <alesf@users.noreply.github.com>
2020-01-31 11:38:28 +0100

    rgp report statement

--- Protocols/EPP/eppExtensions/rgp-1.0/eppRequests/eppRgpRestoreRequest.php
+++ Protocols/EPP/eppExtensions/rgp-1.0/eppRequests/eppRgpRestoreRequest.php
@@ -13,7 +13,7 @@ class eppRgpRestoreRequest extends eppUpdateDomainRequest
      * @param eppDomain|null $removeinfo
      * @param eppDomain|null $updateinfo
      */
-    public function __construct(eppDomain $objectname, $addinfo = null, $removeinfo = null, $updateinfo = null, $type = 'request')
+    public function __construct(eppDomain $objectname, $addinfo = null, $removeinfo = null, $updateinfo = null, $type = 'request', $report = null)
     {
         if ($objectname instanceof eppDomain) {
             $domainname = $objectname->getDomainname();
@@ -29,6 +29,24 @@ class eppRgpRestoreRequest extends eppUpdateDomainRequest
         $rgp->setAttribute('xmlns:rgp', 'urn:ietf:params:xml:ns:rgp-1.0');
         $restore = $this->createElement('rgp:restore');
         $restore->setAttribute('op', $type);
+        if ($type == 'report') {
+            $report = $this->createElement('rgp:report');
+            $preData = $this->createElement('rgp:preData', $report['preData']);
+            $postData = $this->createElement('rgp:postData', $report['postData']);
+            $delTime = $this->createElement('rgp:delTime', $report['resTime']);
+            $resTime = $this->createElement('rgp:resTime', $report['resTime']);
+            $resReason = $this->createElement('rgp:resReason', 'Registrant error.');
+            $statement = $this->createElement('rgp:statement', 'This registrar has not restored the ' . $domainname . ' in order to assume the rights to use or sell the ' . $domainname . ' for itself or for any third party.');
+            $statement2 = $this->createElement('rgp:statement', 'The information in this report is true to best of this registrar\'s knowledge, and this registrar acknowledges that intentionally supplying false information in this report shall constitute an incurable material breach of the Registry-Registrar Agreement.');
+            $report->appendChild($preData);
+            $report->appendChild($postData);
+            $report->appendChild($delTime);
+            $report->appendChild($resTime);
+            $report->appendChild($resReason);
+            $report->appendChild($statement);
+            $report->appendChild($statement2);
+            $restore->appendChild($report);
+        }
         $rgp->appendChild($restore);
         $this->getExtension()->appendChild($rgp);
         $this->addSessionId();
