bb93b985f212968fca6c4bc5b20343bda6e9e508
djurred <38034337+djurred@users.noreply.github.com>
2021-07-09 15:28:03 +0200

    Remove optional null value
    As of PHP 8.0.0, passing mandatory arguments after optional arguments is deprecated. Therefore the directory parameter should (and is) always be passed to the loadSettings function.

--- Protocols/EPP/eppConnection.php
+++ Protocols/EPP/eppConnection.php
@@ -1237,7 +1237,7 @@ class eppConnection {
      * @return array
      * @throws eppException
      */
-    static function loadSettings($directory = null, $settingsfile) {
+    static function loadSettings($directory, $settingsfile) {
         if ($directory) {
             $path = $directory . '/' . $settingsfile;
         } else {
