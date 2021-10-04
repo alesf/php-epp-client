d20ac29e770c676fdc23981617f0561c68b4f8f1
Ewout de Graaf <ewout@metaregistrar.com>
2021-10-04 18:38:04 +0200

    Renamed duplicate functions euridEppInfoDomainRequest and euridEppInfoDomainResponse to authEppInfoDomainRequest and authEppInfoDomainResponse

--- Protocols/EPP/eppExtensions/authInfo-1.1/eppResponses/authEppInfoDomainResponse.php
+++ Protocols/EPP/eppExtensions/authInfo-1.1/eppResponses/authEppInfoDomainResponse.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace Metaregistrar\EPP;
 
-class euridEppInfoDomainResponse extends eppInfoDomainResponse
+class authEppInfoDomainResponse extends eppInfoDomainResponse
 {
-    public function __construct() {
+    function __construct()
+    {
         parent::__construct();
     }
 
@@ -12,7 +14,8 @@ class euridEppInfoDomainResponse extends eppInfoDomainResponse
      *
      * @return array eppContactHandles
      */
-    public function getDomainContacts() {
+    public function getDomainContacts()
+    {
         $xpath = $this->xPath();
         $cont = null;
         $result = $xpath->query('/epp:epp/epp:response/epp:resData/domain:infData/domain:contact');
@@ -42,7 +45,8 @@ class euridEppInfoDomainResponse extends eppInfoDomainResponse
      * Get the date until the auth code is valid
      * @return null|string
      */
-    public function getAuthorisationCodeValidDate() {
+    public function getAuthorisationCodeValidDate()
+    {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/authInfo:infData/authInfo:validUntil');
         if ($result->length > 0) {
@@ -56,7 +60,8 @@ class euridEppInfoDomainResponse extends eppInfoDomainResponse
      *
      * @return boolean
      */
-    public function getQuarantined() {
+    public function getQuarantined()
+    {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/eurid:ext/eurid:infData/eurid:domain/eurid:quarantined');
         if ($result->length > 0) {
@@ -74,7 +79,8 @@ class euridEppInfoDomainResponse extends eppInfoDomainResponse
      *
      * @return boolean
      */
-    public function getOnHold() {
+    public function getOnHold()
+    {
         $xpath = $this->xPath();
         $result = $xpath->query('/epp:epp/epp:response/epp:extension/eurid:ext/eurid:infData/eurid:domain/eurid:onhold');
         if ($result->length > 0) {
