d2a738a063642bb12257d1878012a670303f6c16
alesf <alesf@users.noreply.github.com>
2022-02-09 14:20:46 +0100

    fix extensive quot

--- Protocols/EPP/eppExtensions/idnLang-1.0/eppRequests/verisignEppCreateDomainRequest.php
+++ Protocols/EPP/eppExtensions/idnLang-1.0/eppRequests/verisignEppCreateDomainRequest.php
@@ -3,7 +3,7 @@ namespace Metaregistrar\EPP;
 
 class verisignEppCreateDomainRequest extends verisignEppNamestoreCreateDomainRequest
 {
-    
+
     protected $idnlangextension = null;
     protected $idnlang = null;
 
@@ -15,15 +15,15 @@ class verisignEppCreateDomainRequest extends verisignEppNamestoreCreateDomainReq
     }
 
     private function setIDNLangExtension($language)
-    {        
-        if ($this->getExtension()->getElementsByTagName('idnLang:tag')->count() > 0) 
-        {                        
+    {
+        if ($this->getExtension()->getElementsByTagName('idnLang:tag')->count() > 0)
+        {
             $this->getExtension()->removeChild($this->getExtension()->getElementsByTagName('idnLang:tag')->item(0));
         }
         $this->idnlangextension = $this->createElement('idnLang:tag', $language);
         $this->idnlangextension->setAttribute('xmlns:idnLang', 'http://www.verisign.com/epp/idnLang-1.0');
         $this->idnlangextension->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-        $this->idnlangextension->setAttribute('xsi:schemaLocation', 'http://www.verisign-grs.com/epp/idnLang-1.0 idnLang-1.0.xsd"');
+        $this->idnlangextension->setAttribute('xsi:schemaLocation', 'http://www.verisign-grs.com/epp/idnLang-1.0 idnLang-1.0.xsd');
         $this->getExtension()->appendChild($this->idnlangextension);
         return $this->idnlangextension;
     }
